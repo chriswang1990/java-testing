6577a56e028715ba00a8cc1a6f4f593c7cee5c26
chriswang1990 <1990chriswang1990@gmail.com>
2016-09-29 12:07:59 -0700

    Add solutions for String permutation II

--- leetcode/AddTwoNumberII.java
+++ leetcode/AddTwoNumberII.java
@@ -1,7 +1,13 @@
 package testing.leetcode;
 
 /**
- * Created by 1990c on 9/28/2016.
+ You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in
+ forward order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and
+ returns the sum as a linked list.
+
+ Example
+ Given 6->1->7 + 2->9->5. That is, 617 + 295.
+ Return 9->1->2. That is, 912.
  */
 public class AddTwoNumberII {
   /**
---
+++ leetcode/StringPermutationII.java
@@ -0,0 +1,59 @@
+package testing.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ Given a string, find all permutations of it without duplicates.
+ Example
+
+ Given "abb", return ["abb", "bab", "bba"].
+ Given "aabb", return ["aabb", "abab", "baba", "bbaa", "abba", "baab"].
+ */
+public class StringPermutationII {
+  /**
+   * @param str a string
+   * @return all permutations
+   */
+  public static List<String> stringPermutation2(String str) {
+    // Write your code here
+    List<String> ans = new ArrayList<String>();
+    if (str == null || str.length() == 0) {
+      return ans;
+    }
+    char[] array = str.toCharArray();
+    Arrays.sort(array);
+    str = new String(array);
+    String cur = "";
+    boolean[] visited = new boolean[str.length()];
+    for (int i = 0; i < str.length(); i++) {
+      visited[i] = false;
+    }
+    helper(str, cur, ans, visited);
+    return ans;
+  }
+
+  public static void helper(String str, String cur, List<String> ans, boolean[] visited) {
+    int len = str.length();
+    if (cur.length() == len) {
+      ans.add(cur);
+      return;
+    }
+    for (int i = 0; i < len; i++) {
+      char c = str.charAt(i);
+      if (visited[i] || (i != 0 && str.charAt(i - 1) == c && !visited[i - 1])) {
+        continue;
+      }
+      visited[i] = true;
+      cur += c;
+      helper(str, cur, ans, visited);
+      cur = cur.substring(0, cur.length() - 1);
+      visited[i] = false;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(stringPermutation2("abb"));
+  }
+}
