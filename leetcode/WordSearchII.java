cf6020d088a90476b283148901135726993fa6d8
Minquan Wang <1990chriswang1990@gmail.com>
2017-01-25 23:47:21 -0800

    Add question description to word search II

--- leetcode/WordSearchII.java
+++ leetcode/WordSearchII.java
@@ -5,7 +5,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 /**
- * Created by cwang on 1/25/2017.
+ Given a matrix of lower alphabets and a dictionary. Find all words in the dictionary that can be found in the matrix.
+ A word can start from any position in the matrix and go left/right/up/down to the adjacent position.
+
+ Example
+ Given matrix:
+ doaf
+ agai
+ dcan
+ and dictionary:
+ {"dog", "dad", "dgdg", "can", "again"}
+
+ return {"dog", "dad", "can", "again"}
  */
 public class WordSearchII {
 
